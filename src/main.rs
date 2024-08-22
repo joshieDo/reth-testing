869c70940758ed0834373c847677df97c8e26e70
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-08-22 17:16:32 +0200

    use experimental engine

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 use clap::Parser;
-use reth::cli::Cli;
+use reth::{builder::EngineNodeLauncher, cli::Cli, providers::providers::BlockchainProvider2};
 use reth_engine_tree::tree::TreeConfig;
-use reth_node_ethereum::EthereumNode;
+use reth_node_ethereum::{node::EthereumAddOns, EthereumNode};
 
 mod exex;
 use exex::exex;
@@ -35,7 +35,9 @@ fn main() {
     Cli::<TestArgs>::parse()
         .run(|builder, args| async move {
             let handle = builder
-                .node(EthereumNode::default())
+                .with_types_and_provider::<EthereumNode, BlockchainProvider2<_>>()
+                .with_components(EthereumNode::components())
+                .with_add_ons::<EthereumAddOns>()
                 .extend_rpc_modules(move |ctx| {
                     ctx.modules.merge_configured(rpc_ext.into_rpc())?;
                     Ok(())
@@ -53,7 +55,13 @@ fn main() {
                         args.num_blocks,
                     ))
                 })
-                .launch()
+                .launch_with_fn(|builder| {
+                    let launcher = EngineNodeLauncher::new(
+                        builder.task_executor().clone(),
+                        builder.config().datadir(),
+                    );
+                    builder.launch_with(launcher)
+                })
                 .await?;
 
             handle.wait_for_node_exit().await
