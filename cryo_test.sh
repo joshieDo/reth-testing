1f53844b27ddaf8f1a35fa0056d39caaac65e93f
joshieDo <93316087+joshieDo@users.noreply.github.com>
2024-08-22 21:36:12 +0200

    cryo ranges are exclusive, so +1 blockend on inmem

--- cryo_test.sh
+++ cryo_test.sh
@@ -51,7 +51,7 @@ if [ "$#" -eq 0 ]; then
         exit 1
     fi
     BLOCK_START=$(($(echo $status | jq -r '.result.in_memory_first') - 2))
-    BLOCK_END=$(echo $status | jq -r '.result.tip')
+    BLOCK_END=$(($(echo $status | jq -r '.result.tip') + 1))
 else
     BLOCK_START=$1
     BLOCK_END=$2
